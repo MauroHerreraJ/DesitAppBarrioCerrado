b4643b333471da904b36f6250bad164df987ec3f
MauroHerreraJ <maurodesit@gmail.com>
2024-12-19 15:57:08 -0300

    politics

--- screen/Welcome.js
+++ screen/Welcome.js
@@ -1,29 +1,26 @@
-import { View, Text, Image, StyleSheet, Platform, ImageBackground, Pressable } from "react-native";
-
+import { View, Text, Image, StyleSheet, Platform, ImageBackground, Pressable, ScrollView } from "react-native";
+import { useState } from "react";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 function Welcome({ navigation }) {
 
     function pressHandler() {
         navigation.navigate("Configuration");
     }
+    const [isChecked, setIsChecked] = useState(false); // Estado del checkbox
+
+    function toggleCheckbox() {
+        setIsChecked((prevState) => !prevState); // Alterna entre seleccionado y no seleccionado
+    }
+
+
     return (
         <>
             <ImageBackground
                 source={require('../assets/126353.jpg')}
                 resizeMode="cover"
                 style={styles.rootScreen}>
-                <View>
-                    <View style={styles.textContainer}>
-                        <Text style={styles.text}>Bienvenido!</Text>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <Pressable
-                            onPress={pressHandler} 
-                            style={styles.button}
-                        >
-                            <Text style={styles.textButton}>Continuar</Text>
-                        </Pressable>
-                    </View>
+                
                     <View style={styles.imageContainer}>
                         <Image source={require("../assets/logonuevo.png")}
                             style={{ width: 70, height: 70 }} />
@@ -31,18 +28,93 @@ function Welcome({ navigation }) {
                     <View>
                         <Text style={styles.textImage}>Desit SA</Text>
                     </View>
-                </View>
+                  
+                        <ScrollView style={styles.listContainer}>
+                            <Text style={styles.politicText}>Términos y condiciones de uso</Text>
+                            <Text style={styles.politicText}>
+                                Es necesario, para la configuración y uso de esta aplicación, que lea y acepte los términos y condiciones que a continuación se detallan:
+                                1.1 - Pulsador de Pánico
+                                Es una aplicación destinada al envío de mensajes de emergencia hacia la central de monitoreo
+                                instalada en su barrio, urbanización, ciudad, etc., haciendo uso de los recursos de seguridad con los que cuenta su entorno de aplicación para la atención y/o resolución del evento.
+
+                                1.2 - Condiciones previas para el correcto funcionamiento
+                                Para hacer uso de la APP de emergencia, usted deberá contar con el permiso del organismo que previamente haya instalado el Sistema de Monitoreo de Desit SA. Esta aplicación funciona en conjunto con dicho sistema.
+
+                                1.3 - Configuración inicial
+                                Para comenzar a utilizar la aplicación usted deberá completar tres campos, el primero con el código de licencia, el segundo con el número de equipo y el tercero con el número de cuenta, todos provistos por el organismo de control de implementación de uso de esta APP.
+                                Al ingresar los datos correctos, la aplicación puede solicitar permisos que usted deberá aceptar, de lo contrario la aplicación quedará sin funcionar.
+                                Para mayor detalle consultar el instructivo de instalación de la APP que le envió el organismo de control de uso y aplicación de este sistema.
+                                Una vez hecho esto la aplicación quedará lista para su uso.
+                                Nota: Los datos con los que configuró el envío de eventos hacia la central son ÚNICOS por cada APP individual.
+
+                                Uso y cuidados
+                                Para utilizar la aplicación ante una situación de emergencia usted simplemente deberá abrirla y mantener presionado durante 1 (un) segundo el botón de aviso del evento que quiera comunicar. La aplicación no tiene límite de eventos que pueden ser enviados.
+                                Al enviar un evento a la central de monitoreo se dará aviso que desde su smartphone existe una emergencia de acuerdo a la naturaleza del botón que pulsó dentro de los disponibles en su APP. El pulsador NO ENVÍA información sobre su posición mediante el uso de GPS.
+                                Contamos con su entendimiento y compromiso de utilizar solo en casos de emergencia los botones de emergencia, como así también instruir de forma correcta al resto de los miembros de su familia en especial a los más jóvenes.
+                                Nota: Para poder enviar eventos de forma correcta usted deberá contar con disponibilidad de servicio de internet y/o paquete de datos, ya que cada evento se envía mediante un mensaje vía Internet.
+
+
+                                Formas de envío de alerta
+                                La aplicación cuenta con la capacidad de enviar la alerta vía Internet.
+
+                                Costos
+                                El envío de eventos por IP (internet) corre por parte del servicio de telefonía y/o paquete de datos que usted tenga contratado por lo que cada evento enviado tendrá el costo de la tarifa vigente de su proveedor.
+
+                                Responsabilidades
+                                Desit SA no se hace responsable por fallas en envíos de eventos ocasionadas por causas ajenas al propio funcionamiento de la APP.
+
+                                Política de privacidad
+
+                                La presente Política de Privacidad establece los términos en que Desit SA usa y protege la información que es proporcionada por sus usuarios al momento de utilizar Docta Pánico. Desit SA está comprometido con la seguridad de los datos de sus usuarios y aseguramos que los mismos serán empleados de acuerdo con los términos de este documento.
+                                Sin embargo, esta Política de Privacidad puede cambiar sin previo aviso por lo que le recomendamos revisar estos términos después de cada actualización para asegurarse que está de acuerdo con estos potenciales cambios.
+
+                                1.1 - Información recogida
+                                Desit SA no recoge información guardada en la aplicación ni tampoco recoge información sobre el uso de la misma, toda la información introducida por parte del usuario queda almacenada de manera local en el dispositivo y no es enviada ni a Desit SA ni a un tercero por parte de Desit SA.
+
+                                1.2 – Uso de la información recogida
+                                Desit SA no recoge información de la aplicación ni de su uso, por lo que no procesamos ningún tipo de información personal.
+
+                                1.3 - Divulgación a Terceros
+                                Desit Pánico no comparte información sobre la aplicación con terceros ni tampoco hacemos uso de enlaces hacia terceros dentro de la aplicación.
+
+                                1.4 - Control de su información personal
+                                Toda la información que se ingrese a la aplicación queda almacenada de manera local, como así también en el servidor de Desit con el fin de generar y resguardar la licencia de uso de la app. Los números de teléfonos o textos ingresados sólo serán resguardados a tal fin y bajo ningún concepto serán remitidos a ningún otro destino o empresa mediante Desit SA. El usuario acepta estas condiciones al realizar la configuración de la aplicación en su smartphone.
+
+
+                                Desit SA Se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento
+                            </Text>
+                        </ScrollView>              
+                    <View style={styles.container}>
+                        <Pressable
+                            onPress={toggleCheckbox}
+                            style={[styles.checkbox, isChecked && styles.checked]}
+                        >
+                            {isChecked && <Text style={styles.checkmark}>✓</Text>}
+                        </Pressable>
+                        <Text style={styles.label}>Aceptar términos y condiciones</Text>
+                    </View>
+
+
+                    <View style={styles.buttonContainer}>
+                        <Pressable
+                            onPress={pressHandler}
+                            style={styles.button}
+                        >
+                            <Text style={styles.textButton}>Continuar</Text>
+                        </Pressable>
+                    </View>
+
+               
             </ImageBackground>
         </>
     )
 }
 
+
 export default Welcome;
 
 const styles = StyleSheet.create({
-    textContainer: {
-        padding: 25,
-    },
+
     text: {
         padding: 16,
         fontSize: 30,
@@ -51,14 +123,14 @@ const styles = StyleSheet.create({
         marginTop: 80
     },
     buttonContainer: {
-        marginTop: 350
+        marginTop: 10
     },
     button: {
         padding: 16,
         margin: 8,
         borderRadius: 8,
-        paddingVertical: 25,
-        paddingHorizontal: 71,
+        paddingVertical: 18,
+        paddingHorizontal: 50,
         overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
         backgroundColor: '#0d47a1',
         elevation: 4,
@@ -76,15 +148,55 @@ const styles = StyleSheet.create({
     rootScreen: {
         flex: 1
     },
-   imageContainer:{
-    alignItems:"center",
-    marginTop:80,
-    marginBottom:5
-   },
-   textImage: {
-    textAlign: "center",
-    fontSize: 14,
-    color:"white"
-   }
+    imageContainer: {
+        alignItems: "center",
+        marginTop: 80,
+        marginBottom: 5
+    },
+    textImage: {
+        textAlign: "center",
+        fontSize: 14,
+        color: "white",
+        marginBottom:10
+    },
+    politicText: {
+        padding: 10,
+        fontSize: 14,
+        color: "white",
+        marginTop: 10,
+        textAlign: 'justify'
+    },
+
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        margin: 10,
+    },
+    checkbox: {
+        width: 24,
+        height: 24,
+        borderWidth: 2,
+        borderColor: "#0d47a1",
+        borderRadius: 4,
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 10,
+    },
+    checked: {
+        backgroundColor: "#0d47a1",
+    },
+    checkmark: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    label: {
+        fontSize: 16,
+        color: "white",
+    },
+    listContainer: {
+        flex: 1,
+    },
+    
 })
 
